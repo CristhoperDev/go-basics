134fa8594cdff57af0d523bd74b3c26e09dff654
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-02-02 10:23:17 -0500

    variables

---
+++ 02/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//var name, lastName string
+	//name := "Cristhoper"
+	//lastName := "Alvarez"
+	name, lastName := "Cristhoper", "Alvarez"
+
+	name, age := "Pedro", 20
+
+	fmt.Println(name, lastName, age)
+}
