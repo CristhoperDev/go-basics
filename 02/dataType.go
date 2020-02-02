dd0eae863c0473a0e3a98015fb929ab08e9074f4
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-02-02 11:20:22 -0500

    data types

---
+++ 02/dataType.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*var a int
+	var b int8
+	name := "Cristhoper"
+	lastName := "Alvarez"
+	age := 21
+	a = 121212
+	b = 5
+
+	// casting
+	c := a + int(b)
+	fmt.Println(c)
+	fmt.Printf("Hello %s %s %d how are you?\n", lastName, name, age)
+	fmt.Printf("b is type of: %T\n", b)*/
+	//arithmetic priority
+	// + - * /
+	// () % * / + -
+	//modulo is the residue of division
+	// 7 % 3 = 1
+	//d := 6 + 6*(6-6)
+	//fmt.Println(d)
+
+	//zero value
+	var name string
+	var number int
+	var flag bool
+	fmt.Println(name, number, flag)
+}
